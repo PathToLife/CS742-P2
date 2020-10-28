9f44f1488e28ea7cec3ca5060fff5cdbb33d06dd
Sean Zeng <contact@seanzeng.com>
2020-10-28 21:31:53 +1300

    update yucheng code #2

--- src/simple.tcl
+++ src/simple.tcl
@@ -11,8 +11,7 @@ $ns namtrace-all $fd
 #Group1
 set n0 [$ns node]
 $n0 color green
-$n0 shape box
-$ns at 0.0 "$n0 label Root"
+$n0 shape hexagon
 set n1 [$ns node]
 $n1 color green
 $n1 shape hexagon
@@ -21,7 +20,8 @@ $n2 color green
 $n2 shape hexagon
 set n3 [$ns node]
 $n3 color green
-$n3 shape hexagon
+$n3 shape box
+$ns at 0.0 "$n3 label Root"
 set n4 [$ns node]
 $n4 color green
 $ns at 0.0 "$n4 label Gateway"
@@ -29,14 +29,14 @@ $ns at 0.0 "$n4 label Gateway"
 #Group2
 set n5 [$ns node]
 $n5 color blue
-$n5 shape box
-$ns at 0.0 "$n5 label Root"
+$n5 shape hexagon
 set n6 [$ns node]
 $n6 color blue
 $n6 shape hexagon
 set n7 [$ns node]
 $n7 color blue
-$n7 shape hexagon
+$n7 shape box
+$ns at 0.0 "$n7 label Root"
 set n8 [$ns node]
 $n8 color blue
 $n8 shape hexagon
@@ -47,8 +47,7 @@ $ns at 0.0 "$n9 label Gateway"
 #Group3
 set n10 [$ns node]
 $n10 color red
-$n10 shape box
-$ns at 0.0 "$n10 label Root"
+$n10 shape hexagon
 set n11 [$ns node]
 $n11 color red
 $n11 shape hexagon
@@ -57,7 +56,8 @@ $n12 color red
 $n12 shape hexagon
 set n13 [$ns node]
 $n13 color red
-$n13 shape hexagon
+$n13 shape box
+$ns at 0.0 "$n13 label Root"
 set n14 [$ns node]
 $n14 color red
 $ns at 0.0 "$n14 label Gateway"
@@ -88,14 +88,14 @@ $ns duplex-link $n7 $n9 1.5Mb 10ms DropTail
 
 #Group3
 $ns duplex-link $n10 $n11 1.5Mb 10ms DropTail
-$ns duplex-link $n10 $n12 1.5Mb 10ms DropTail
 $ns duplex-link $n12 $n13 1.5Mb 10ms DropTail
+$ns duplex-link $n10 $n12 1.5Mb 10ms DropTail
 $ns duplex-link $n13 $n11 1.5Mb 10ms DropTail
 $ns duplex-link $n13 $n14 1.5Mb 10ms DropTail
 
 #Link-Gateway-Together
-$ns duplex-link $n4 $n9 15Mb 99ms DropTail
-$ns duplex-link $n9 $n14 15Mb 99ms DropTail
+$ns duplex-link $n4 $n9 15Mb 70ms DropTail
+$ns duplex-link $n9 $n14 15Mb 70ms DropTail
 
 
 $ns duplex-link-op $n0 $n1 orient right
@@ -136,25 +136,24 @@ $ns duplex-link-op $n11 $n13 orient down
 
 set group1 [Node allocaddr]
 set mproto1 CtrMcast
-
 set mrthandle1 [$ns mrtproto $mproto1 {}]
-$mrthandle1 set_c_rp $n0
+$mrthandle1 set_c_rp $n3
 
 set group2 [Node allocaddr]
 set mproto2 CtrMcast
 #set mproto2 ST
 set mrthandle2 [$ns mrtproto $mproto2 {}]
-$mrthandle2 set_c_rp $n5
+$mrthandle2 set_c_rp $n7
 
 set group3 [Node allocaddr]
 set mproto3 CtrMcast
 #set mproto3 ST
 set mrthandle3 [$ns mrtproto $mproto3 {}]
-$mrthandle3 set_c_rp $n10
+$mrthandle3 set_c_rp $n13
 
 # UDP Transport agent for the traffic source for group1
 set udp0 [new Agent/UDP]
-$ns attach-agent $n0 $udp0
+$ns attach-agent $n3 $udp0
 $udp0 set dst_addr_ $group1
 $udp0 set dst_port_ 0
 set cbr1 [new Application/Traffic/CBR]
@@ -162,7 +161,7 @@ $cbr1 attach-agent $udp0
 
 # Transport agent for the traffic source for group2
 set udp1 [new Agent/UDP]
-$ns attach-agent $n5 $udp1
+$ns attach-agent $n7 $udp1
 $udp1 set dst_addr_ $group2
 $udp1 set dst_port_ 0
 set cbr2 [new Application/Traffic/CBR]
@@ -170,7 +169,7 @@ $cbr2 attach-agent $udp1
 
 # Transport agent for the traffic source for group3
 set udp2 [new Agent/UDP]
-$ns attach-agent $n10 $udp2
+$ns attach-agent $n13 $udp2
 $udp2 set dst_addr_ $group3
 $udp2 set dst_port_ 0
 set cbr3 [new Application/Traffic/CBR]
@@ -183,8 +182,8 @@ $ns attach-agent $n1 $rcvr1
 $ns at 1.0 "$n1 join-group $rcvr1 $group1"
 $ns attach-agent $n2 $rcvr1
 $ns at 1.0 "$n2 join-group $rcvr1 $group1"
-$ns attach-agent $n3 $rcvr1
-$ns at 1.0 "$n3 join-group $rcvr1 $group1"
+$ns attach-agent $n0 $rcvr1
+$ns at 1.0 "$n0 join-group $rcvr1 $group1"
 $ns attach-agent $n4 $rcvr1
 $ns at 1.0 "$n4 join-group $rcvr1 $group1"
 
@@ -192,8 +191,8 @@ $ns at 1.0 "$n4 join-group $rcvr1 $group1"
 set rcvr2 [new Agent/Null]
 $ns attach-agent $n6 $rcvr2
 $ns at 1.0 "$n6 join-group $rcvr2 $group2"
-$ns attach-agent $n7 $rcvr2
-$ns at 1.0 "$n7 join-group $rcvr2 $group2"
+$ns attach-agent $n5 $rcvr2
+$ns at 1.0 "$n5 join-group $rcvr2 $group2"
 $ns attach-agent $n8 $rcvr2
 $ns at 1.0 "$n8 join-group $rcvr2 $group2"
 $ns attach-agent $n9 $rcvr2
@@ -203,10 +202,10 @@ $ns at 1.0 "$n9 join-group $rcvr2 $group2"
 set rcvr3 [new Agent/Null]
 $ns attach-agent $n11 $rcvr3
 $ns at 1.0 "$n11 join-group $rcvr3 $group3"
+$ns attach-agent $n10 $rcvr3
+$ns at 1.0 "$n10 join-group $rcvr3 $group3"
 $ns attach-agent $n12 $rcvr3
 $ns at 1.0 "$n12 join-group $rcvr3 $group3"
-$ns attach-agent $n13 $rcvr3
-$ns at 1.0 "$n13 join-group $rcvr3 $group3"
 $ns attach-agent $n14 $rcvr3
 $ns at 1.0 "$n14 join-group $rcvr3 $group3"
 
@@ -226,9 +225,9 @@ $ns at 1.0 "$n14 join-group $rcvr3 $group3"
 
 $ns at 0.0 "$cbr1 start"
 $ns at 9.5 "$cbr1 stop"
-$ns at 0.0 "$cbr2 start"
+$ns at 0.1 "$cbr2 start"
 $ns at 9.5 "$cbr2 stop"
-$ns at 0.0 "$cbr3 start"
+$ns at 0.2 "$cbr3 start"
 $ns at 9.5 "$cbr3 stop"
 
 #post-processing
